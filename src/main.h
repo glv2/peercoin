48768832dd395d8e9001156d73a50ab07dc9ff5a
Giannis Dzegoutanis <erasmospunk@gmail.com>
2014-07-07 17:35:32 +0200

    added getrawmempool rpc call

--- src/main.h
+++ src/main.h
@@ -1781,6 +1781,7 @@ public:
                 bool fCheckInputs, bool* pfMissingInputs);
     bool addUnchecked(CTransaction &tx);
     bool remove(CTransaction &tx);
+    void queryHashes(std::vector<uint256>& vtxid);
 
     unsigned long size()
     {
