6e09a5f6d36b229dcd1f4ca04ea899d97775360e
sandakersmann <sandakersmann@users.noreply.github.com>
2015-04-20 05:15:06 +0200

    Add TheSeven's dnsseed
    TheSeven has kindly provided an auto dns seed service for peercoin. This change adds TheSeven's dnsseed to the dnsseed list.

--- src/net.cpp
+++ src/net.cpp
@@ -1141,6 +1141,7 @@ void MapPort(bool /* unused fMapPort */)
 static const char *strDNSSeed[][2] = {
     {"seed", "seed.ppcoin.net"},
     {"seedppc", "seedppc.ppcoin.net"},
+    {"7server", "ppcseed.ns.7server.net"},
     {"altcointech", "dnsseed.ppc.altcointech.net"},
     {"diandianbi", "seed.diandianbi.org"},
     {"tnseed", "tnseed.ppcoin.net"},
